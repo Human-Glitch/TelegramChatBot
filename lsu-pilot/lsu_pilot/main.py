92c4ddbe5e358cc2e70fc5912a02de5802e6d0b4
Kody Buss <kody.buss@zelis.com>
2024-10-28 19:08:23 -0500

    Updated chat bot to support answering questions from mozilla MDN docs

--- lsu-pilot/lsu_pilot/main.py
+++ lsu-pilot/lsu_pilot/main.py
@@ -3,29 +3,34 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from openai import OpenAI
-from dotenv import load_dotenv
+import pandas as pd
+import numpy as np
 
-load_dotenv()  # take environment variables from .env.
+from questions import answer_question
 
-tg_bot_token = os.getenv("TG_BOT_TOKEN")
-openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+openai = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+tg_bot_token = os.environ['TG_BOT_TOKEN']
+
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the absolute path to the CSV file
+csv_path = os.path.join(current_dir, "processed", "embeddings.csv")
+df = pd.read_csv(csv_path, index_col=0)
+df["embeddings"] = df["embeddings"].apply(eval).apply(np.array)
 
 messages = [{
-  "role": "system",
-  "content": "You are a helpful assistant that answers questions."
+    "role": "system",
+    "content": "You are a helpful assistant that answers questions."
 }]
 
 logging.basicConfig(
-  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-  level=logging.INFO)
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  await context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="I'm a bot, please talk to me!")
-  
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
   messages.append({"role": "user", "content": update.message.text})
-  completion = openai.chat.completions.create(model="gpt-3.5-turbo",
+  completion = openai.chat.completions.create(model="gpt-4o-mini",
                                               messages=messages)
   completion_answer = completion.choices[0].message
   messages.append(completion_answer)
@@ -33,11 +38,23 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
   await context.bot.send_message(chat_id=update.effective_chat.id,
                                  text=completion_answer.content)
 
+async def mozilla(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  answer = answer_question(df, question=update.message.text, debug=True)
+  await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  await context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="I'm a bot, please talk to me!")
+
 if __name__ == '__main__':
   application = ApplicationBuilder().token(tg_bot_token).build()
 
   start_handler = CommandHandler('start', start)
   chat_handler = CommandHandler('chat', chat)
+
+  mozilla_handler = CommandHandler('mozilla', mozilla)
+  application.add_handler(mozilla_handler)
+
   application.add_handler(start_handler)
   application.add_handler(chat_handler)
 
